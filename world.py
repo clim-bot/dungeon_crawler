880fc5a2e1a975ae519f44c8b107b104f9921e9f
Chris Lim <toffer.lim87@gmail.com>
2025-02-09 10:45:00 -0800

    automated level generation

--- world.py
+++ world.py
@@ -1,11 +1,18 @@
-import pygame
+from character import Character
+from items import Item
 import constants
+
 
 class World():
     def __init__(self):
         self.map_tiles = []
+        self.obstacle_tiles = []
+        self.exit_tile = None
+        self.item_list = []
+        self.player = None
+        self.charcter_list = []
 
-    def process_data(self, data, tile_list):
+    def process_data(self, data, tile_list, item_images, mob_animations):
         self.level_length = len(data)
         # iterate through each value in the data file
         for y, row in enumerate(data):
@@ -16,6 +23,34 @@ class World():
                 image_y = y * constants.TILE_SIZE
                 image_rect.center = (image_x, image_y)
                 tile_data = [image, image_rect, image_x, image_y]
+
+                # add obstacle tile data (wallls, etc.)
+                if tile == 7:
+                    self.obstacle_tiles.append(tile_data)
+                elif tile == 8:
+                    self.exit_tile = tile_data
+                elif tile == 9:
+                    coin = Item(image_x, image_y, 0, item_images[0])
+                    self.item_list.append(coin)
+                    tile_data[0] = tile_list[0]
+                elif tile == 10:
+                    potion = Item(image_x, image_y, 1, [item_images[1]])
+                    self.item_list.append(potion)
+                    tile_data[0] = tile_list[0]
+                elif tile == 11:
+                    player = Character(image_x, image_y, 100, mob_animations, 0, False, 1)
+                    self.player = player
+                    tile_data[0] = tile_list[0]
+                elif tile >= 12 and tile <= 16:
+                    enemy = Character(image_x, image_y, 100, mob_animations, tile - 11, False, 1)
+                    self.charcter_list.append(enemy)
+                    tile_data[0] = tile_list[0]
+                elif tile == 17:
+                    boss = Character(image_x, image_y, 200, mob_animations, 1, True, 2)
+                    self.charcter_list.append(boss)
+                    tile_data[0] = tile_list[0]
+
+
 
                 # add image data to main tile list
                 if tile >= 0:
